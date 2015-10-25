1b789271686dec17d07078921bb669171ecc4812
David Dworken <david@daviddworken.com>
2015-10-24 23:39:15 -0400

    Initial version

---
+++ gpgBruteForce.py
@@ -0,0 +1,40 @@
+#!/bin/python
+from subprocess import check_output as run
+try: 
+    from progress.bar import Bar
+    progressBar = True
+except:
+    progressBar = False
+import argparse
+
+def getWordlist(wordlistFilename):
+    with open(wordlistFilename) as f:
+        return f.read().splitlines()
+
+def isCorrectPassword(password, filename):
+    try:
+        output = run("gpg --batch --yes --passphrase " + password + " -d " + filename + " 2>&1", shell=True)   #shell injection potential, do not run on untrusted input
+        if "decryption failed:" in output:
+            return False
+        else:
+            return True
+    except:
+        return False
+
+parser = argparse.ArgumentParser(description='GPG Brute Forcer')
+parser.add_argument('-f','--filename', help='Location of the GPG encrypted file', required=True)
+parser.add_argument('-w','--wordlistFilename', help='Location of the wordlist', required=True)
+args = parser.parse_args()
+
+filename = args.filename
+wordlist = getWordlist(args.wordlistFilename)
+
+if progressBar: bar = Bar("Brute Forcing...", max=len(wordlist))
+for password in wordlist:
+    if isCorrectPassword(password, filename):
+        if progressBar: bar.finish()
+        print "The password is: " + password
+        exit()
+    if progressBar: bar.next()
+if progressBar: bar.finish()
+print "Failed to find the password, expand your wordlist and try again"
